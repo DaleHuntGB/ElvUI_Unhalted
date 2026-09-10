f2c69d1bf163201eda0c9cc309a988c2a06edafa
Dale Hunt <dalehunt@gmail.com>
2026-09-10 16:03:56 +0100

    Use this instead of our way.

--- Modules/AutoDelete.lua
+++ Modules/AutoDelete.lua
@@ -1,5 +1,11 @@
 local Private = select(2, ...)
 
+local function EasyDelete_OnShow(frame)
+    if not Private.DB.global.QualityOfLife.Toggles.AutoDelete then return end
+	frame.EditBox:SetText(DELETE_ITEM_CONFIRM_STRING)
+end
+
 function Private:SetupAutoDelete()
-    hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"], "OnShow", function(self) if self.EditBox and Private.DB.global.QualityOfLife.Toggles.AutoDelete then self.EditBox:SetText("DELETE") end end)
+    hooksecurefunc(StaticPopupDialogs.DELETE_GOOD_ITEM, 'OnShow', EasyDelete_OnShow)
+	hooksecurefunc(StaticPopupDialogs.DELETE_GOOD_QUEST_ITEM, 'OnShow', EasyDelete_OnShow)
 end
