d1a7df836d07282c012c6ccf51a29dedaa3b0154
Dale Hunt <dalehunt@gmail.com>
2026-09-15 20:57:45 +0100

    I don't think this needs to exist.

--- Core/GUI.lua
+++ Core/GUI.lua
@@ -22,7 +22,7 @@ function Private:CreateGUI()
     local GUI = Private.GUI
     local DB = Private.DB.global
 
-    GUI = ACH:Group(format("|T%s:18:18|t%s", "Interface\\AddOns\\ElvUI_Unhalted\\Media\\Logo_64.png", Private.AddOnName), nil, 20, "tree")
+    GUI = ACH:Group(format("%s", Private.AddOnName), nil, 20, "tree")
 
     --#region - AddOn Skins
 
