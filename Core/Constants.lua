fad334ed00b8bd552fb41b6b1ab7ae22f81cb1f3
Dale Hunt <dalehunt@gmail.com>
2026-09-22 21:19:20 +0100

    Track Hearty Well Fed

--- Core/Constants.lua
+++ Core/Constants.lua
@@ -117,7 +117,7 @@ Private.RaidBuffs = {
 }
 
 Private.PersonalBuffs = {
-    ["Food"] = { spellNames = { "Well Fed" }, iconID = 136000, alwaysShow = false },
+    ["Food"] = { spellNames = { "Well Fed", "Hearty Well Fed" }, iconID = 136000, alwaysShow = false },
     ["Flask"] = { spellNames = { "Flask of the Shattered Sun", "Flask of the Blood Knights", "Flask of the Magisters", "Flask of Thalassian Resistance" }, iconID = 7548903, alwaysShow = false },
     ["Oils"] = { iconID = 135641, dualWieldSpecIDs = { [259] = true, [260] = true, [261] = true, [263] = true, [72] = true }, alwaysShow = false },
     ["Source of Magic"] = { spellNames = { "Source of Magic" }, iconID = 4630412, alwaysShow = false },
